e86dd1abc2a1ed2f90ffc358018e5d2e572f1e1c
= <mikaiyl.davis@icloud.com>
2018-12-20 00:57:43 -0500

    freeze

--- molecule.py
+++ molecule.py
@@ -1,3 +1,5 @@
+import copy
+
 '''ARGPARSE'''
 
 '''
@@ -27,7 +29,7 @@ create_map(LIST OF FOUR STRINGS)
 
 
 def create_board():
-    board = [i for i in [['' for k in range(10)] for j in range(10)]]
+    board = [i for i in [[' ' for k in range(12)] for j in range(12)]]
     return board
 
 
@@ -40,26 +42,73 @@ Usage: add_list_to_board(0, 'AAAAAAAA', board)
 def add_list_to_board(index, charlist, board):
     # index 0-11 map to side ascending 12-23 map to columns across
     # return new board with added list if list fits else none
+    b = copy.deepcopy(board)
+
     if len(charlist) is not 12:
-        print '@add_list_to_board, charlist is not 10 chars'
-        return board
+        print '@add_list_to_board, charlist is not 12 chars'
+        return None
 
     if index > 0 and index < 11:
         for i, char in enumerate(charlist):
-            if board[index][i] == char or board[index][i] == '':
+            if board[index][i] == char or board[index][i] == ' ':
                 board[index][i] = char
             else:
-                return board
+                return b
     elif index > 12 and index < 23:
         for i, char in enumerate(charlist):
-            if board[i][index-12] == char or board[i][index-12] == '':
+            if board[i][index-12] == char or board[i][index-12] == ' ':
                 board[i][index-12] = char
             else:
-                return board
+                return b
     else:
-        pass
-
+        return None
     return board
+
+
+''' add_list_to_board:
+Does just that. It adds a list of charachters to an existing
+Usage: add_list_to_board(0, 'AAAAAAAA', board)
+'''
+
+
+def find_space_on_board(charlist, board):
+    # index 0-11 map to side ascending 12-23 map to columns across
+    # return new board with added list if list fits else none
+    matching_rows = []
+    matching_cols = []
+
+    buffer = []
+
+    if len(charlist) is not 12:
+        print '@add_list_to_board, charlist is not 12 chars'
+        return None
+
+    for index in range(0, 23):
+        if index > 0 and index < 11:
+            for i, char in enumerate(charlist):
+                if board[index][i] == char or board[index][i] == ' ':
+                    pass
+                elif i == 0:
+                    buffer += [i-1, i, i+1]
+                    break
+                else:
+                    break
+
+            matching_rows.append(index)
+        elif index > 12 and index < 23:
+            for i, char in enumerate(charlist):
+                if board[i][index-12] == char or board[i][index-12] == ' ':
+                    pass
+                elif i == 0:
+                    buffer += [i-1, i, i+1]
+                    break
+                else:
+                    break
+            matching_cols.append(index)
+        else:
+            pass
+
+    return filter(lambda x: x not in buffer, matching_rows + matching_cols)
 
 
 '''
@@ -82,20 +131,36 @@ def match_strings(a, b, c, d):
         # apply each ramaining items to position two
         # loop though positions for b
         # save matching positions to queue if they exist
-    answers = {}
+    # answers = []
+    s_list = [b, c, d]
 
-    for down in range(10):
-        layer_count = 0
-        board = create_board()
-        board = add_list_to_board(down+1, a, board)
-        for across in range(10):
-            pass
-
+    for down in range(1, 11):
+        _board_ = create_board()
+        board = add_list_to_board(down, a, _board_)
+        for across in range(12, 23):
+            print('\nrow: {}, column: {}\n\n'.format(down, across))
+            for side_2 in s_list:
+                b2 = add_list_to_board(across, side_2, board)
+                print_map(b2)
+                del(b2)
+                # if b2 is not None:
+        del(board)
 
     # filter through queue and check for position three
     # filter through queue and check for position four
     # for items still in queue, tally space in middle and return largest
-    pass
+
+
+''' HELPERS '''
+
+
+def print_map(m):
+    if m is None:
+        print 'Empty'
+    else:
+        for line in m:
+            print line
+
 
 
 '''DISPLAY RESULT'''
